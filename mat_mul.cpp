8cc726d838acbb25a6217e344a17376d59765f6a
rhornbuckle3 <russspringfield@gmail.com>
2019-10-03 15:18:00 -0400

    pps finished

---
+++ mat_mul.cpp
@@ -0,0 +1,48 @@
+#include <omp.h> //parallel library
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> //for log2 and pow and ceil
+#include <time.h> //for performance comparisons
+//clang++ -Xpreprocessor -fopenmp mat_mul.cpp -o pps -lomp 
+//^macOS
+
+//targeting N time complexity with a large number of threads
+int main(int argc,  char *argv[]){
+    int nthreads, tid;
+    int N=8; //input size
+    int P=N*N; //number of threads
+    int *C,*A,*B;
+    
+
+    A=new int[N*N];
+    B=new int[N*N];
+    C=new int[N*N];
+
+    for(int i=0;i<N*N;i++){
+        A[i]=(rand()*clock())%50;
+        B[i]=(rand()*clock())%50;
+        C[i]=(rand()*clock())%50;
+        }
+
+    omp_set_num_threads(P);
+    #pragma omp parallel
+    {
+        int sum=0;
+        int pid=omp_get_thread_num();
+        int ci=pid/N,cj=pid%N;
+        for(int i=0;i<N;i++){
+            sum+=A[ci*N+i]*B[cj+N*i];
+        }
+        C[ci*N+cj]=sum;
+    }
+    for(int i=0;i<N*N;i++) {
+        printf("c: %d",C[i]);
+        if(i%N==0){
+            printf("\n");
+        }
+    }
+}
+int alt(int A,int B,int C){
+    int P=16;
+
+}
--- parallel_prefix_sum.cpp
+++ parallel_prefix_sum.cpp
@@ -4,66 +4,101 @@
 #include <math.h> //for log2 and pow and ceil
 #include <time.h> //for performance comparisons
 //clang++ -Xpreprocessor -fopenmp parallel_prefix_sum.cpp -o pps -lomp 
+//^macOS
 
 int main(int argc,  char *argv[]){
     int nthreads, tid;
-    int N=1024; //input size
-    int P=N/2; //number of threads
-
-    //following two lines for console input control over threads and input -- untested and dangerous like anything science-y in a super hero movie that's not iron man
-    //Maybe that's one of the reasons behind the break out success of the original iron man? A super hero movie where the mad genius messing with technology is the solution rather than the problem was a real breath of fresh air coming out of the raimi spider-man years.
-    //if(argc>1) P=atoi(argv[1]);
-    //if(argc>2) N=atoi(argv[2]);
-
-    long* a;
-    long* b;
-    a=new long[N];
-    b=new long[N];
-    int i;
-    //filling input with random integers
-    for(i=0;i<N;i++){
-        long rand_num=(rand()*clock())%50;
-        a[i]=rand_num;
-        b[i]=rand_num;
+    int N,P;
+    float st_macro,pt_macro;
+    if(argc>1){
+        N=atoi(argv[1]);
+    }else{
+        N=16; //input size
     }
-    //Printing the input
-    printf("%s","Input Array\n");
-    for(i=0;i<N;i++){
-        printf("%d",(int)a[i]);
-        printf("%s","\n");
+    if(argc>2){
+        P=atoi(argv[2]);
+    }else{
+        P=N/2; //number of threads
     }
-    //serial prefix sum
-    for(i=0;i<7;i++){
-        printf("%d",(int)b[i]);
-        printf("%s","\n");
-    }
-    for(i=1;i<7;i++){
-        b[i]=b[i-1]+b[i];
-    }
-    //serial output
-    printf("%s","Output Array for serial\n");
-    for(i=0;i<7;i++){
-        printf("%d",(int)b[i]);
-        printf("%s","\n");
-    }
-    //parallel prefix sum
-    omp_set_num_threads(P);
-    for(int j=0;j<ceil(log2(N));j++){
-        for(i=1;i<(N/j+1)/P;i++){
-            #pragma omp parallel private(nthreads, tid)
-            {
-                tid=omp_get_thread_num();
-                    if((i*P+tid-pow(2,j))>=0){
-                        a[i*P+tid]=a[i*P+tid]+a[int(i*P+tid-pow(2,j))];
-                    }
-            }
+    int local_size=floor(N/P);
+    
+    //b for serial, a and c for parallel
+    //runs 30 times to get a decent average running time for each input size
+    for(int q=0;q<30;q++){
+        clock_t serial_time,parallel_time;
+        long *a,*b,*c;
+        a=new long[N];
+        b=new long[N];
+        c=new long[N];
+        //filling input with random integers
+        for(int i=0;i<N;i++){
+            long rand_num=(rand()/(clock())%50);
+            a[i]=rand_num;
+            b[i]=rand_num;
+            c[i]=rand_num;
         }
+        //Printing the input
+        //printf("%s","Input Array\n");
+        //for(i=0;i<N;i++){
+        //    printf("%d : %ld\n",i,a[i]);
+        //}
+        //serial prefix sum
+        serial_time=clock();
+        for(int i=1;i<N;i++) b[i]=b[i-1]+b[i];
+        serial_time=clock()-serial_time;
+        //parallel prefix sum
+        parallel_time=clock();
+        omp_set_num_threads(P);
+        int n=N-1;
+        for(int j=0;j<(int)ceil(log2(N));j++){
+            int j_offset=pow(2,j);
+            int ops=N-j_offset;
+            #pragma omp parallel private(nthreads, tid)
+                {  
+                tid=omp_get_thread_num();
+                int ops_per_run=floor(ops/P);
+                for(int i=0;i<ops_per_run;i++){
+                //ops here 
+                int pos=n-(i*P+tid);
+                //printf("C[%d](%ld)+A[%d](%ld)=%ld \n",pos,c[pos],pos-j_offset,a[pos-j_offset],c[pos]+a[pos-j_offset]);
+                    c[pos]=c[pos]+a[pos-j_offset];
+                }
+                if(ops%P>tid){
+                    int pos=n-(ops_per_run*P+tid);
+                    //printf("C[%d](%ld)+A[%d](%ld)=%ld \n",pos,c[pos],pos-j_offset,a[pos-j_offset],c[pos]+a[pos-j_offset]);
+                    c[pos]=c[pos]+a[pos-j_offset];
+                    //cycle extra ops here
+                }
+                //updating reference array in parallel (very slow)
+                //#pragma omp barrier
+                //for(int i=0;i<local_size;i++) a[i*P+tid]=c[i*P+tid];
+                //if(N%P>tid) a[local_size*P+tid]=c[local_size*P+tid];
+            }
+            //updating reference array in serial
+            for(int i=0;i<N;i++) a[i]=c[i];
+        }
+        parallel_time=clock()-parallel_time;
+        if(q==0){
+            pt_macro=(float)parallel_time;
+            st_macro=(float)serial_time;
+        }else{
+            pt_macro+=(float)parallel_time;
+            st_macro+=(float)serial_time;
+        }
+        //printing the output
+        //printf("%s","Output Array for serial\n");
+        //for(i=0;i<N;i++)printf("%d\n",(int)b[i]);
+        //printf("%s","Output Array for parallel\n");
+        //for(i=0;i<N;i++) printf("%d %ld\n",(int)a[i],a[i]-b[i]);
+        //printf("clocks per second: %d\n",CLOCKS_PER_SEC);
+        //printf("serial time in seconds: %f\n",(float)(serial_time)/CLOCKS_PER_SEC);
+        //printf("parallel time in seconds: %f\n",(float)(parallel_time)/CLOCKS_PER_SEC);
+        //printf("easy copy: %f\n",((float)(serial_time)/CLOCKS_PER_SEC)/((float)(parallel_time)/CLOCKS_PER_SEC));
     }
-    //printing the output
-    printf("%s","Output Array for parallel\n");
-    for(i=0;i<N;i++){
-        printf("%d",(int)a[i]);
-        printf("%s","\n");
-    }
+    pt_macro=(float)pt_macro/30;
+    st_macro=(float)st_macro/30;
+    printf("serial time: %f\n",st_macro);
+    printf("parallel time: %f\n",pt_macro);
+    printf("easy copy: %f\n",(float)st_macro/(float)pt_macro);
 return 0;
 }
