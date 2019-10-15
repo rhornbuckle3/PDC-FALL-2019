8d6b9cc4a83a6d7c38e890a2745d95ad342688ee
rhornbuckle3 <russspringfield@gmail.com>
2019-10-15 16:28:45 -0400

    Location fixed

--- location.cpp
+++ location.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
-int location(double ***array,int row, int col, int depth){
-    int depth_num = sizeof(array[0][0]);
-    int row_num = sizeof(array[0])/(depth_num/sizeof(double));
-    int col_num = sizeof(array)/(depth_num/sizeof(double))*(row_num/sizeof(double));
-    int locale = 2000 + row * col_num * depth_num + col * depth_num + depth;
+int location(double ***array,int row, int col, int depth, int row_num, int col_num,int depth_num){
+    //can't calculate dimensions with a pointer array unfortunately
+    int locale = 2000 + (row * col_num * depth_num + col * depth_num + depth)*sizeof(array);
     return locale;
 }
 double *** array_constructor(int rows , int cols, int depth){
     double ***array;
     array = new double**[rows];
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < rows; i++){
         array[i] = new double*[cols];
-        for(int j = 0; j < 5; j++){
+        for(int j = 0; j < cols; j++){
             array[i][j] = new double[depth];
+            //could fill it here with another loop
         }
     }
     return array;
 }
 int main(){
     double ***array = array_constructor(3,2,5);
-    cout << location(array,1,1,4);
+    cout << location(array,1,1,4,3,2,5);
     cout << '\n';
     return 0;
 }
